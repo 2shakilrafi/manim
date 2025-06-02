2497e2533bfe365332879e0c0c7693ba20a0dd24
Shakil Rafi <shakilrafi@Mac.attlocal.net>
2025-06-01 21:16:58 -0500

    more commits

---
+++ ggplot.py
@@ -0,0 +1,65 @@
+from manim import *
+
+class ggplot(Scene):
+    def construct(self):
+        english_text = Text("She sells seashells by the seashore", font_size=22, font="Iosevka Term")
+        self.play(
+            AddTextLetterByLetter(english_text)
+        )
+        self.wait(0.5)
+
+        underline_1 = Line(start=english_text.get_left(), end=english_text.get_left() + RIGHT * 0.52, color=RED).shift(DOWN * 0.2)
+        underline_2 = Line(start=english_text.get_left() + RIGHT * 0.8, end=english_text.get_left() + RIGHT * 1.75, color=BLUE).shift(DOWN * 0.2)
+        underline_3 = Line(start=english_text.get_left() + RIGHT * 2.0, end=english_text.get_left() + RIGHT * 3.75, color=GREEN).shift(DOWN * 0.2)
+        underline_4 = Line(start=english_text.get_left() + RIGHT * 4.0, end=english_text.get_left() + RIGHT * 4.35, color=ORANGE).shift(DOWN * 0.2)
+        underline_5 = Line(start=english_text.get_left() + RIGHT * 4.6, end=english_text.get_left() + RIGHT * 5.15, color=PURPLE).shift(DOWN * 0.2)
+        underline_6 = Line(start=english_text.get_left() + RIGHT * 5.4, end=english_text.get_left() + RIGHT * 7.0, color=PINK).shift(DOWN * 0.2)
+
+        self.play(
+            Write(underline_1),
+            Write(underline_2),
+            Write(underline_3),
+            Write(underline_4),
+            Write(underline_5),
+            Write(underline_6),
+            run_time = 1.0
+        )
+
+        subject_label = Text("Subject", font_size=12, font="Iosevka Term").next_to(underline_1, DOWN)
+        verb_label = Text("Verb", font_size=12, font="Iosevka Term").next_to(underline_2, DOWN)
+        object_label = Text("Object", font_size=12, font="Iosevka Term").next_to(underline_3, DOWN)
+        preposition_label = Text("Preposition", font_size=12, font="Iosevka Term").next_to(underline_4, DOWN)
+        location_label = Text("Location", font_size=12, font="Iosevka Term").next_to(underline_5, UP* 2.1)
+        noun_label = Text("Noun", font_size=12, font="Iosevka Term").next_to(underline_6, DOWN)
+
+        self.play(
+            Create(subject_label),
+            Create(verb_label),
+            Create(object_label),
+            Create(preposition_label),
+            Create(location_label),
+            Create(noun_label),
+            run_time = 1.0
+        )
+        self.wait(0.5)
+
+        self.play(
+            Uncreate(subject_label),
+            Uncreate(verb_label),
+            Uncreate(object_label),
+            Uncreate(preposition_label),
+            Uncreate(location_label),
+            Uncreate(noun_label),
+            run_time = 0.5
+        )
+
+        self.play(
+            FadeOut(underline_1),
+            FadeOut(underline_2),
+            FadeOut(underline_3),
+            FadeOut(underline_4),
+            FadeOut(underline_5),
+            FadeOut(underline_6),
+            Uncreate(english_text)
+        )
+        self.wait(0.25)
