04401fc44a275552a7f57c50f511b44aefd052b6
Shakil Rafi <shakilrafi@Mac.attlocal.net>
2025-06-07 20:30:30 -0500

    more animations added

--- ggplot.py
+++ ggplot.py
@@ -25,12 +25,12 @@ class ggplot(Scene):
             run_time = 1.0
         )
 
-        subject_label = Text("Subject", font_size=12, font="Iosevka Term").next_to(underline_1, DOWN)
-        verb_label = Text("Verb", font_size=12, font="Iosevka Term").next_to(underline_2, DOWN)
-        object_label = Text("Object", font_size=12, font="Iosevka Term").next_to(underline_3, DOWN)
-        preposition_label = Text("Preposition", font_size=12, font="Iosevka Term").next_to(underline_4, DOWN)
-        location_label = Text("Location", font_size=12, font="Iosevka Term").next_to(underline_5, UP* 2.1)
-        noun_label = Text("Noun", font_size=12, font="Iosevka Term").next_to(underline_6, DOWN)
+        subject_label = Text("Subject", font_size=12, font="Iosevka Term", color = RED).next_to(underline_1, DOWN)
+        verb_label = Text("Verb", font_size=12, font="Iosevka Term", color = BLUE).next_to(underline_2, DOWN)
+        object_label = Text("Object", font_size=12, font="Iosevka Term", color = GREEN).next_to(underline_3, DOWN)
+        preposition_label = Text("Preposition", font_size=12, font="Iosevka Term", color = ORANGE).next_to(underline_4, DOWN)
+        location_label = Text("Location", font_size=12, font="Iosevka Term", color = PURPLE).next_to(underline_5, UP* 2.1)
+        noun_label = Text("Noun", font_size=12, font="Iosevka Term", color = PINK).next_to(underline_6, DOWN)
 
         self.play(
             Create(subject_label),
@@ -62,4 +62,73 @@ class ggplot(Scene):
             FadeOut(underline_6),
             Uncreate(english_text)
         )
+        self.wait(0.25)
+
+        ggplot_text_1 = Text("From the iris dataset",font_size=18, font="Iosevka Term").move_to(ORIGIN + UP * 2)
+        ggplot_text_2 = Text("plot Sepal Length vs Sepal width", font_size=18, font="Iosevka Term").next_to(ggplot_text_1, DOWN)
+        ggplot_text_3 = Text("using points", font_size=18, font="Iosevka Term").next_to(ggplot_text_2, DOWN)
+        ggplot_text_4 = Text("with Minimal Theme", font_size=18, font="Iosevka Term").next_to(ggplot_text_3, DOWN)
+        # ggplot_text_5 = Text("with title 'Iris Data', x-label 'Sepal Length' and y-label 'Sepal Width'", font_size=18, font="Iosevka Term").next_to(ggplot_text_4, DOWN)
+
+        self.play(
+            AddTextLetterByLetter(ggplot_text_1),
+            AddTextLetterByLetter(ggplot_text_2),
+            AddTextLetterByLetter(ggplot_text_3),
+            AddTextLetterByLetter(ggplot_text_4),
+            # AddTextLetterByLetter(ggplot_text_5),
+            run_time = 1.5
+        )
+
+        self.wait(1.0)
+
+        self.play(
+            ggplot_text_1.animate.align_to(LEFT * config.frame_width / 2 + 0.25, LEFT),
+            ggplot_text_2.animate.align_to(LEFT * config.frame_width / 2 + 0.25, LEFT),
+            ggplot_text_3.animate.align_to(LEFT * config.frame_width / 2 + 0.25, LEFT),
+            ggplot_text_4.animate.align_to(LEFT * config.frame_width / 2 + 0.25, LEFT)
+        )
+
+        gg_underline_1 = Line(start=ggplot_text_1.get_left(), end=ggplot_text_1.get_right(), color=RED).shift(DOWN * 0.2)
+        gg_underline_2 = Line(start=ggplot_text_2.get_left(), end=ggplot_text_2.get_right(), color=BLUE).shift(DOWN * 0.2)
+        gg_underline_3 = Line(start=ggplot_text_3.get_left(), end=ggplot_text_3.get_right(), color=GREEN).shift(DOWN * 0.2)
+        gg_underline_4 = Line(start=ggplot_text_4.get_left(), end=ggplot_text_4.get_right(), color=ORANGE).shift(DOWN * 0.2)
+
+        self.play(
+            Create(gg_underline_1),
+            Create(gg_underline_2),
+            Create(gg_underline_3),
+            Create(gg_underline_4)
+        )
+
+        data_label = Text("Data", font_size=12, font="Iosevka Term", color = RED).next_to(gg_underline_1, RIGHT)
+        aesthetics_label = Text("Aesthetics", font_size=12, font="Iosevka Term", color = BLUE).next_to(gg_underline_2, RIGHT)
+        geometry_label = Text("Geometry", font_size=12, font="Iosevka Term", color = GREEN).next_to(gg_underline_3, RIGHT)
+        theme_label = Text("Theme", font_size=12, font="Iosevka Term", color = ORANGE).next_to(gg_underline_4, RIGHT)
+
+        self.play(
+            Create(data_label),
+            Create(aesthetics_label),
+            Create(geometry_label),
+            Create(theme_label)
+        )
+        self.wait(1.0)
+
+        r_code_1 = Text("iris |>",font_size=18, font="Iosevka Term", color = RED).move_to(ORIGIN + UP * 2 + RIGHT * 0.5)
+        r_code_2 = Text("ggplot(aes(Sepal.Length, Sepal.Width)) + ",font_size=18, font="Iosevka Term", color = BLUE).next_to(r_code_1, DOWN).align_to(r_code_1, LEFT)
+        r_code_3 = Text("geom_point() + ",font_size=18, font="Iosevka Term", color = GREEN).next_to(r_code_2, DOWN).align_to(r_code_2, LEFT)
+        r_code_4 = Text("theme_minimal()",font_size=18, font="Iosevka Term", color = ORANGE).next_to(r_code_3, DOWN).align_to(r_code_3, LEFT)
+
+        self.play(
+            AddTextLetterByLetter(r_code_1)
+        )
+        self.play(
+            AddTextLetterByLetter(r_code_2)
+        )
+        self.play(
+            AddTextLetterByLetter(r_code_3)
+        )
+        self.play(
+            AddTextLetterByLetter(r_code_4)
+        )
+
         self.wait(0.25)
